a1e983e9bbe17eae503145cc6df0cd8b76aafeb3
Seonho Kim <dev.sensational@gmail.com>
2023-03-09 17:28:28 +0900

    Add function
    Delete Class
    - ObjectArrange

--- Assets/Scripts/UnityGridStyle.cs
+++ Assets/Scripts/UnityGridStyle.cs
@@ -2,12 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class UnityGridStyle : MonoBehaviour
-{
-    protected int cellWidth { get; set; }
-    protected int cellHeight { get; set; }
-    protected int maxHorizontal { get; set; }
-    protected int maxVertical { get; set; }
+public class UnityGridStyle : MonoBehaviour {
+    [Range(0,2)]public float cellWidth;
+    [Range(0, 2)] public float cellHeight;
+    [Range(0, 30)] public int maxHorizontal;
 
-    protected abstract void sortCell();
+    [SerializeField] private List<Transform> objectList = new List<Transform>();
+
+    private int childCount = 0;
+
+
+    void Awake() {
+        Transform tf = GetComponent<Transform>();
+        childCount = tf.childCount;
+
+        for (int i = 0; i < childCount; i++) {
+            objectList.Add(tf.GetChild(i));
+        }
+    }
+
+    void Start() {
+        sortCell();
+    }
+
+    protected void sortCell() {
+        float posX = 0, posZ = 0;
+        for (int i = 0; i < childCount; i++) {
+            Transform ptr = objectList[i];
+            ptr.localPosition = new Vector3(posX, 1, posZ);
+            posX += ptr.GetComponent<RectTransform>().rect.width + cellWidth;
+            if ((i + 1) % maxHorizontal == 0) {
+                posZ -= ptr.GetComponent<RectTransform>().rect.height + cellHeight;
+                posX = 0;
+            }
+        }
+    }
 }
